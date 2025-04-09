c28ed4c0bd9ba3c300b3149a54bd3f4e3faf94b6
mcasasarrojo <maria.casasarrojo@usp.ceu.es>
2025-04-09 16:27:14 +0200

    bloodbank pojo

--- src/bloodank/db/pojos/BloodBank.java
+++ src/bloodank/db/pojos/BloodBank.java
@@ -2,4 +2,12 @@ package bloodank.db.pojos;
 
 public class BloodBank {
 
+	//POJO (plain old java object)
+	private int ID;
+	private string address;
+	private string city;
+	private int contact_number;
+	//private Bloodbankmanager bbManager;
+	
+	
 }
