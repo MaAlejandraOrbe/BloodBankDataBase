4f4f9380221f17b0c81df85b80216961fadfcaf7
mcasasarrojo <maria.casasarrojo@usp.ceu.es>
2025-05-09 17:00:19 +0200

    arreglao

--- src/bloodbank/ui/Menu.java
+++ src/bloodbank/ui/Menu.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
 
-import bloodank.db.pojos.*;
+import bloodbank.db.pojos.*;
 import bloodbank.ifaces.*;
 import jdbc.*;
 
