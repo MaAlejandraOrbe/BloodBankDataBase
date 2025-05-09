72566120febd2dd0966b29d29630386d29dffddd
Judyta Kopaniecka <kopanieckajudyta@gmail.com>
2025-05-09 16:48:41 +0200

    Added missing libs

--- src/jdbc/SQLInsertRecipient.java
+++ src/jdbc/SQLInsertRecipient.java
@@ -12,44 +12,55 @@ public class SQLInsertRecipient {
         try {
             Class.forName("org.sqlite.JDBC");
             Connection c = DriverManager.getConnection("jdbc:sqlite:bloodbank_database.db");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             c.createStatement().execute("PRAGMA foreign_keys=ON");
             System.out.println("Database connection opened.");
+            
+            System.out.println("1: New recipient");
+            System.out.println("2: Existing recipient");
+            System.out.print("Your choice (1 or 2): ");
+            int choice = Integer.parseInt(reader.readLine().trim());
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Please, input the Recipient info:");
-            System.out.print("First name: ");
-            String firstName = reader.readLine();
-            System.out.print("Last name: ");
-            String lastName = reader.readLine();
-            System.out.print("DOB (YYYY-MM-DD): ");
-            String dob = reader.readLine();
-            System.out.print("Blood type: ");
-            String bloodType = reader.readLine();
-            System.out.print("Country: ");
-            String country = reader.readLine();
-            System.out.print("Contact number: ");
-            String contact = reader.readLine();
-            System.out.print("Emergency contact number: ");
-            String emergency = reader.readLine();
+            switch(choice) {
+            
+            case 1:
+            	 System.out.println("Please, input the Recipient info:");
+                 System.out.print("First name: ");
+                 String firstName = reader.readLine();
+                 System.out.print("Last name: ");
+                 String lastName = reader.readLine();
+                 System.out.print("DOB (YYYY-MM-DD): ");
+                 String dob = reader.readLine();
+                 System.out.print("Blood type: ");
+                 String bloodType = reader.readLine();
+                 System.out.print("Country: ");
+                 String country = reader.readLine();
+                 System.out.print("Contact number: ");
+                 String contact = reader.readLine();
+                 System.out.print("Emergency contact number: ");
+                 String emergency = reader.readLine();
 
-            Statement stmt = c.createStatement();
-            String sql = "INSERT INTO Recipient "
-                       + "(first_name, last_name, DOB, blood_type, country, contact_number, emergency_contact_number) "
-                       + "VALUES ('"
-                       + firstName  + "', '"
-                       + lastName   + "', '"
-                       + dob        + "', '"
-                       + bloodType  + "', '"
-                       + country    + "', '"
-                       + contact    + "', '"
-                       + emergency  + "');";
-            stmt.executeUpdate(sql);
-            stmt.close();
+                 Statement stmt = c.createStatement();
+                 String sql = "INSERT INTO Recipient "
+                            + "(first_name, last_name, DOB, blood_type, country, contact_number, emergency_contact_number) "
+                            + "VALUES ('"
+                            + firstName  + "', '"
+                            + lastName   + "', '"
+                            + dob        + "', '"
+                            + bloodType  + "', '"
+                            + country    + "', '"
+                            + contact    + "', '"
+                            + emergency  + "');";
+                 stmt.executeUpdate(sql);
+                 stmt.close();
+                 }
 
-            System.out.println("Recipient info processed");
-            System.out.println("Record inserted.");
+                 System.out.println("Recipient info processed");
+                 System.out.println("Record inserted.");
 
-            c.close();
+                 c.close();
+            	
+           
             System.out.println("Database connection closed.");
         } catch (Exception e) {
             e.printStackTrace();
