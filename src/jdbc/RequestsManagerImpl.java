9a5b4181bd4f154fb73b3df0b27c7a421caec29c
MaAlejandraOrbe <mariaalejandra.orbecabello1@usp.ceu.es>
2025-05-10 20:46:29 +0200

    Recipient Manager Impl

---
+++ src/jdbc/RequestsManagerImpl.java
@@ -0,0 +1,46 @@
+package jdbc;
+
+import bloodbank.db.pojos.BloodRequest;
+import bloodbank.db.pojos.Hospital;
+import bloodbank.db.pojos.Recipient;
+import bloodbank.ifaces.RequestsManager;
+
+public class RequestsManagerImpl implements RequestsManager {
+
+	@Override
+	public void fullfillBloodRequest(BloodRequest bloodRequest) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void newBloodRequest(BloodRequest bloodRequest) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void newRecipient(Recipient recipient) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void newHospital(Hospital hospital) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void linkRecipientToBloodRequest(int recipientId, int bloodRequestId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteRecipient(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
