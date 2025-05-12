0377fe5114bae9d0e6169003806007d9715963d2
MaAlejandraOrbe <mariaalejandra.orbecabello1@usp.ceu.es>
2025-05-12 18:27:39 +0200

    changess

--- src/bloodbank/db/pojos/Donation.java
+++ src/bloodbank/db/pojos/Donation.java
@@ -11,64 +11,109 @@ public class Donation implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Integer id;
-	private Integer quantity;
 	private String status;
 	private LocalDate donation_date; //LocalDate fecha = LocalDate.of(1995, 4, 10); (ano, mes, dia)
+	private Integer quantity;
 	private LocalDate expiration_date;
 	private BloodBank bloodbank;
+	private Donor donor;
 	
 	
 	public Donation() {
 		super();
 	}
 	
-	public Donation(Integer id, Integer quantity, String status, LocalDate donation_date, LocalDate expiration_date) {
+	
+	public Donation(Integer id, String status, LocalDate donation_date, Integer quantity, LocalDate expiration_date,
+			BloodBank bloodbank, Donor donor) {
 		super();
 		this.id = id;
-		this.quantity = quantity;
 		this.status = status;
 		this.donation_date = donation_date;
+		this.quantity = quantity;
 		this.expiration_date = expiration_date;
+		this.bloodbank = bloodbank;
+		this.donor = donor;
 	}
 
+
+	
+	
 	public Integer getId() {
 		return id;
 	}
+
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public Integer getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
+
+
 	public String getStatus() {
 		return status;
 	}
+
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
+
 	public LocalDate getDonation_date() {
 		return donation_date;
 	}
+
+
 	public void setDonation_date(LocalDate donation_date) {
 		this.donation_date = donation_date;
 	}
+
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+
 	public LocalDate getExpiration_date() {
 		return expiration_date;
 	}
+
+
 	public void setExpiration_date(LocalDate expiration_date) {
 		this.expiration_date = expiration_date;
 	}
+
+
 	public BloodBank getBloodbank() {
 		return bloodbank;
 	}
+
+
 	public void setBloodbank(BloodBank bloodbank) {
 		this.bloodbank = bloodbank;
 	}
 
-	
+
+	public Donor getDonor() {
+		return donor;
+	}
+
+
+	public void setDonor(Donor donor) {
+		this.donor = donor;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -86,11 +131,15 @@ public class Donation implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
+
 	@Override
 	public String toString() {
-		return "Donation [id=" + id + ", quantity=" + quantity + ", status=" + status + ", donation_date="
-				+ donation_date + ", expiration_date=" + expiration_date + ", bloodbank=" + bloodbank + "]";
+		return "Donation [id=" + id + ", status=" + status + ", donation_date=" + donation_date + ", quantity="
+				+ quantity + ", expiration_date=" + expiration_date + ", bloodbank=" + bloodbank + ", donor=" + donor
+				+ "]";
 	}
+
+	
 	
 	
 	
